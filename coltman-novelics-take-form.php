8544b897decbe4e412a5817cb1c4340c19fca7c1
Juan Carlos Avila <juanvs23@gmail.com>
2021-06-04 06:58:40 -0400

    initital git

---
+++ coltman-novelics-take-form.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name:       Plugin take contact form
+ * Plugin URI:        https://github.com/juanvs23/coltman-novelics-take-form
+ * Description:       Captura del formulario
+ * Version:           1.0.0
+ * Author:            Juan Carlos Avila
+ * Author URI:        https://coltman-desing.tk/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       coltman-novelics-take-form
+ * Domain Path:       /languages
+ */
+
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+// Need to store this variable before leaving this file
+define( 'COLTMAN_VERSION', __FILE__ );
